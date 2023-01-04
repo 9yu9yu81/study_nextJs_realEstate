1db3e243c15f94d4cc1703f1526c416a7210ffd7
9yu9yu81 <9yu9yu81@gmail.com>
2023-01-04 17:23:02 +0900

    Setting: db connect & tailwind & sitemap
    db connect: DB -> planet scale / ORM -> prisma
    tailwind: styling
    sitemap: make "postbuild" script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Myspot</title>
+        <meta name="description" content="My Spot direct transaction" />
+      </Head>
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
